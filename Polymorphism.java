b4d1d7784216f7f298a87dba50ddc8473f07ae58
rajthakre005 <rajthakre005@gmail.com>
2026-05-13 21:00:10 +0530

    Refactor Shape interface and implement area method
    Updated Shape interface to include area method and modified Circle and Rectangle classes to implement area calculation. Enhanced main method to take user input for dimensions and display area.

--- Polymorphism.java
+++ Polymorphism.java
@@ -1,28 +1,53 @@
-interface Shape {
-    void draw();
+import java.util.Scanner;
+interface Shape{
+	void area();
+	void draw();
 }
 class Circle implements Shape{
-    public void draw(){
-        System.out.println("Drawing Circle");
-    }
+	double radius;
+	Circle(double radius){
+	this.radius=radius;
+	}
+	public void area(){
+	double area = 3.14*radius*radius;
+	System.out.println("Area of Circle:"+area);
+	}
+
+	public void draw(){
+		System.out.println("Drawing a Circle");
+	}
 }
 class Rectangle implements Shape{
-    public void draw(){
-        System.out.println("Drawing Rectangle");
-    }
-}
-class Triangle implements Shape{
-    public void draw(){
-        System.out.println("Drawing Triangle");
-    }
+	double length,breadth;
+	Rectangle(double length,double breadth){
+	this.length = length;
+	this.breadth = breadth;
+	}
+	public void area(){
+	double area = length*breadth;
+	System.out.println("Area of Rectangle"+area);
+	}
+
+	public void draw(){
+		System.out.println("Drawing a Rectangle");
+	}
 }
 public class Polymorphism{
-    public static void main(String[] args) {
-        Shape s1 = new Circle();
-        Shape s2 = new Rectangle();
-        Shape s3 = new Triangle();
-        s1.draw();
-        s2.draw();
-        s3.draw();
-    }
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter radius for Circle:");
+	double r = sc.nextDouble();
+	System.out.println("Enter length for Rectangle:");
+	double l = sc.nextDouble();
+	System.out.println("Enter breadth for Rectangle:");
+	double b = sc.nextDouble();
+	
+	Shape s1 = new Circle(r);
+	Shape s2 = new Rectangle(l,b);
+	s1.draw();
+	s1.area();
+	s2.draw();
+	s2.area();
+	
+	}
 }
