28f6f2e70e2d146a304ff78a895044221afa3688
rajthakre005 <rajthakre005@gmail.com>
2026-05-13 22:19:58 +0530

    Update SingleInheritance.java

--- SingleInheritance.java
+++ SingleInheritance.java
@@ -1,32 +1,76 @@
-// Superclass (Parent)
+import java.util.Scanner;
+
 class Animal {
+    protected String name;
+    protected int age;
+
+    Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
     void eat() {
-        System.out.println("Animal is eating...");
+        System.out.println(name + " is eating...");
     }
 
     void sleep() {
-        System.out.println("Animal is sleeping...");
+        System.out.println(name + " is sleeping...");
     }
 }
 
-// Subclass (Child)
+
 class Dog extends Animal {
+    String breed;
+
+    
+    Dog(String name, int age, String breed) {
+        super(name, age);  
+        this.breed = breed;
+    }
+
     void bark() {
-        System.out.println("Dog is barking...");
+        System.out.println(name + " is barking...");
+    }
+
+
+    @Override
+    void eat() {
+        System.out.println(name + " is eating dog food...");
+    }
+
+    void displayDetails() {
+        System.out.println("Dog Name : " + name);
+        System.out.println("Dog Age  : " + age);
+        System.out.println("Breed    : " + breed);
     }
 }
 
-// Main class
-public class SingleInheritance{
+public class SingleInheritance {
     public static void main(String[] args) {
-        // Create object of subclass
-        Dog d = new Dog();
 
-        // Calling superclass methods
-        d.eat();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Dog Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Dog Age: ");
+        int age = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Dog Breed: ");
+        String breed = sc.nextLine();
+
+        Dog d = new Dog(name, age, breed);
+
+        System.out.println("\n--- Dog Details ---");
+        d.displayDetails();
+
+        System.out.println();
+
+        d.eat(); 
         d.sleep();
+        d.bark();  
 
-        // Calling subclass method
-        d.bark();
+        sc.close();
     }
 }
