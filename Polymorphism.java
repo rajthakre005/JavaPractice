734dcfa6fd333a673e72c08f476ad541451f0af8
rajthakre005 <rajthakre005@gmail.com>
2026-05-13 19:46:29 +0530

    Update Polymorphism.java

--- Polymorphism.java
+++ Polymorphism.java
@@ -11,16 +11,6 @@ class Rectangle implements Shape{
         System.out.println("Drawing Rectangle");
     }
 }
-class Hexagon implements Shape{
-    public void draw(){
-        System.out.println("Drawing Hexagon");
-    }
-}
-class Pentagon implements Shape{
-    public void draw(){
-        System.out.println("Drawing Pentagon");
-    }
-}
 class Triangle implements Shape{
     public void draw(){
         System.out.println("Drawing Triangle");
@@ -30,13 +20,9 @@ public class Polymorphism{
     public static void main(String[] args) {
         Shape s1 = new Circle();
         Shape s2 = new Rectangle();
-        Shape s3 = new Hexagon();
-        Shape s4 = new Pentagon();
-        Shape s5 = new Triangle();
+        Shape s3 = new Triangle();
         s1.draw();
         s2.draw();
         s3.draw();
-        s4.draw();
-        s5.draw();
     }
 }
