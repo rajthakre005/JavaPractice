ebec073d0704db7acba9450874611ffccc0bb3d9
rajthakre005 <rajthakre005@gmail.com>
2026-05-13 19:47:17 +0530

    Update abstract_class.java

--- abstract_class.java
+++ abstract_class.java
@@ -11,25 +11,11 @@ class Rectangle extends Shape{
         System.out.println("Drawing Rectangle");
     }
 }
-class Pentagon extends Shape{
-    void draw(){
-        System.out.println("Drawing Pentagon");
-    }
-}
-class Hexagon extends Shape{
-    void draw(){
-        System.out.println("Drawing Hexagon");
-    }
-}
 public class abstract_class{
     public static void main(String[] args){
         Shape s1 = new Circle();
         Shape s2 = new Rectangle();
-        Shape s3 = new Pentagon();
-        Shape s4 = new Hexagon();
         s1.draw();
         s2.draw();
-        s3.draw();
-        s4.draw();
     }
 }
